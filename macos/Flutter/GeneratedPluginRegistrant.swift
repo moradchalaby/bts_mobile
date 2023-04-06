babd5d8fde817fbb2d2cb5d4ef6afb0ee813adce
Murat Çelebi <moradchalaby@gmail.com>
2023-04-06 09:27:36 +0300

    masasüstü alındı

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,7 +7,7 @@ import Foundation
 
 import desktop_window
 import firebase_core
-import path_provider_macos
+import path_provider_foundation
 import share_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
